fbccca16cabb06a76402be6bf6a1f0ccc22ff856
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-04-08 01:55:57 +0100

    Add relevant actions for submissions.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,27 @@
 class SubmissionsController < ApplicationController
+  before_filter :authenticate_user!
+  
+  def new
+    @submission = Submission.new
+  end
+  
+  def create
+    @submission = Submission.new(submission_params)
+    @submission.user_id = current_user.id
+    if @submission.save
+      redirect_to submissions_path
+    else
+      render new_submission_path
+    end
+  end
+  
+  def index
+    @submissions = current_user.submissions
+  end
+end
+
+private
+  
+def submission_params
+  params.require(:submission).permit(:title, :due_date)
 end
